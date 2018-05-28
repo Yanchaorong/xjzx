6f72a8a0f26ca9e55cc069ddb918feebd3628ec6
Ycr <1315247666@qq.com>
2018-05-28 22:22:30 +0800

    搭建框架

---
+++ flask_xjzx/flask_xjzx.py
@@ -0,0 +1,20 @@
+from flask_script import Manager
+
+from app import create_app
+from config import DevelopConfig
+
+app = create_app(DevelopConfig)
+
+manager = Manager(app)
+
+from models import db
+# 初始化数据库
+db.init_app(app)
+#
+from flask_migrate import Migrate, MigrateCommand
+# 自定义命令用于数据库迁移
+Migrate(app, db)
+manager.add_command('bbb', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
